63d467acb2e7c40eef51add57d165dc824c0eafa
Tharik Lourenco <tharik.augusto67@gmail.com>
2020-08-12 18:30:19 -0300

    Stafull

--- lib/screens/contact_form.dart
+++ lib/screens/contact_form.dart
@@ -1,6 +1,16 @@
+import 'package:alura_bytebank/models/contact.dart';
 import 'package:flutter/material.dart';
 
-class ContactForm extends StatelessWidget {
+class ContactForm extends StatefulWidget {
+  @override
+  _ContactFormState createState() => _ContactFormState();
+}
+
+class _ContactFormState extends State<ContactForm> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _accountNumberController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,6 +22,7 @@ class ContactForm extends StatelessWidget {
         child: Column(
           children: [
             TextField(
+              controller: _nameController,
               decoration: InputDecoration(
                   enabledBorder: UnderlineInputBorder(
                       borderSide:
@@ -23,6 +34,7 @@ class ContactForm extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(top: 8.0),
               child: TextField(
+                controller: _accountNumberController,
                 decoration: InputDecoration(
                     enabledBorder: UnderlineInputBorder(
                         borderSide:
@@ -38,8 +50,14 @@ class ContactForm extends StatelessWidget {
               child: SizedBox(
                 width: double.maxFinite,
                 child: RaisedButton(
-                  onPressed: () => null,
                   child: Text("Adiconar"),
+                  onPressed: () {
+                    final String name = _nameController.text;
+                    final int conta =
+                        int.tryParse(_accountNumberController.text);
+                    final Contact newContact = Contact(name, conta);
+                    Navigator.pop(context, newContact);
+                  },
                 ),
               ),
             )
