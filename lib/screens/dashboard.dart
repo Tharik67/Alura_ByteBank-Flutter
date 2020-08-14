a95fc11785b3998e55f8ccccea1dd8c788474fae
Tharik Lourenco <tharik.augusto67@gmail.com>
2020-08-14 18:04:26 -0300

    refatoracao botao

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:alura_bytebank/screens/contacts_list.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 
 class Dashboard extends StatelessWidget {
   @override
@@ -13,48 +14,91 @@ class Dashboard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(16.0),
             child: Image.asset(
               'images/bytebank_logo.png',
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Material(
-              color: Theme.of(context).primaryColor,
-              child: InkWell(
-                onTap: () => Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => ContactsList(),
-                  ),
+          Container(
+            height: 120,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                _FeatureIten(
+                  "Transferir",
+                  Icons.monetization_on,
+                  onClick: () => _showContactsList(context),
                 ),
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  margin: EdgeInsets.all(8),
-                  height: 100,
-                  width: 150,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Center(
-                        child: Icon(
-                          Icons.people,
-                          color: Colors.white,
-                          size: 40,
-                        ),
-                      ),
-                      Text(
-                        "Contatos",
-                        style: TextStyle(color: Colors.white, fontSize: 16),
-                      )
-                    ],
-                  ),
+                _FeatureIten(
+                  "Extrato",
+                  Icons.description,
+                  onClick: () => _showExtrato(context),
                 ),
-              ),
+              ],
             ),
-          )
+          ),
         ],
+      ),
+    );
+  }
+
+  void _showContactsList(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => ContactsList(),
+      ),
+    );
+  }
+
+  void _showExtrato(BuildContext context) {
+    print('Extrato');
+    // Navigator.of(context).push(
+    //   MaterialPageRoute(
+    //     builder: (context) => ContactsList(),
+    //   ),
+    // );
+  }
+}
+
+class _FeatureIten extends StatelessWidget {
+  final String name;
+  final IconData icone;
+  final Function onClick;
+
+  const _FeatureIten(this.name, this.icone, {@required this.onClick});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: () => onClick(),
+          child: Container(
+            padding: EdgeInsets.all(8),
+            margin: EdgeInsets.all(8),
+            height: 100,
+            width: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: Icon(
+                    icone,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                ),
+                Text(
+                  name,
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
